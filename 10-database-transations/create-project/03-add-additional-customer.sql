75a762bac8b0f52467c01757917594d58e58b81d
Bharat Sonwane <bharatrsonwane@gmail.com>
2023-08-26 00:58:31 +0530

    10-database-transations

---
+++ 10-database-transations/create-project/03-add-additional-customer.sql
@@ -0,0 +1,19 @@
+INSERT INTO customers(
+    first_name,
+    last_name,
+    email
+)
+VALUES(
+    'Maria',
+    'Jones',
+    'maria@test.com'
+);
+
+-- wrong query
+INSERT INTO orders(
+   amount_billed,
+   customer_id 
+)
+VALUES(
+    103.12
+);
