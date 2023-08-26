841ced157a8e987e0e5523f7b58ad40231f1c300
Bharat Sonwane <bharatrsonwane@gmail.com>
2023-08-26 23:51:00 +0530

    09

--- 09-build-in-query-functions/create-project/02-create-table.sql
+++ 09-build-in-query-functions/create-project/02-create-table.sql
@@ -1,8 +1,22 @@
+-- -- postgresql
+-- CREATE TABLE memberships (
+--     id SERIAL PRIMARY KEY,
+--     membership_start DATE,
+--     membership_end DATE,
+--     last_checkin TIMESTAMP,
+--     last_checkout TIMESTAMP,
+--     consumption NUMERIC(5, 2),
+--     first_name VARCHAR(200),
+--     last_name VARCHAR(200),
+--     price NUMERIC(5, 2),
+--     billing_frequency INT,
+--     gender VARCHAR(200)
+-- );
+
+
+-- MySQl
 CREATE TABLE memberships (
-    -- for mysql use this code
-    -- id INT PRIMARY KEY AUTO_INCREMENT,
-    -- for postgresql use this code
-    -- id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     membership_start DATE,
     membership_end DATE,
     last_checkin TIMESTAMP,
--- 09-build-in-query-functions/create-project/04-create-additional-tables.sql
+++ 09-build-in-query-functions/create-project/04-create-additional-tables.sql
@@ -1,18 +1,28 @@
+-- postgresql
+
 CREATE TABLE customers(
-    -- for mysql use this code
-    -- id INT PRIMARY KEY AUTO_INCREMENT,
-    -- for postgresql use this code
-    -- id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(200),
     last_name VARCHAR(200),
     email VARCHAR(200)
 );
 
 CREATE TABLE orders(
-    -- for mysql use this code
-    -- id INT PRIMARY KEY AUTO_INCREMENT,
-    -- for postgresql use this code
-    -- id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    amount_billed NUMERIC(5, 2),
+    customer_id INT REFERENCES customers
+);
+
+-- MySQl
+CREATE TABLE customers(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(200),
+    last_name VARCHAR(200),
+    email VARCHAR(200)
+);
+
+CREATE TABLE orders(
+    id INT PRIMARY KEY AUTO_INCREMENT,
     amount_billed NUMERIC(5, 2),
     customer_id INT REFERENCES customers
 );
